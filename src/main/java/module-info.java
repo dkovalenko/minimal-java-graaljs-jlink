5053fd295cc1bf17bcfd3c1737c44935ada4809a
Dmitriy Kovalenko <dmitriy.kovalenko@megogo.net>
2021-04-21 19:25:14 +0300

    add graavm.js module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.sample.jlink {
     requires org.slf4j;
     requires org.graalvm.sdk;
     requires org.graalvm.truffle;
+    requires org.graalvm.js;
     exports com.sample.app;
     // export com.sample.app to com.specific.package;
 }
